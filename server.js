da2ed8dbc2f50822b0fa0baa981c8d385af3514b
Dev <devvarshney11@gmail.com>
2024-03-20 19:32:00 +0530

    Some Changes

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const router = require("./routes/index");
 const app = express();
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/", require("./routes/index"));
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
